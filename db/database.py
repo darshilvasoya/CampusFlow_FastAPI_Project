309da33ad8513b90ec4ae1c63a4c4a7bad94c58f
darshilvasoya <darshilvy@gmail.com>
2025-09-21 04:46:19 +0530

    Fix IndentationError in db/database.py

--- db/database.py
+++ db/database.py
@@ -1,6 +1,6 @@
 from contextlib import contextmanager
 import mysql.connector
-
+import os
 
 @contextmanager
 def get_db_cursor(commit=False):
@@ -8,27 +8,24 @@ def get_db_cursor(commit=False):
     cursor = None
     try:
         connection = mysql.connector.connect(
-            host="campusflow.mysql.pythonanywhere-services.com",
-            user="campusflow",
-            password="czon@2510",
-            database="campusflow$student_database",
-            autocommit=False  # Important: handle transactions manually
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("DB_NAME"),
+            autocommit=False
         )
         cursor = connection.cursor(dictionary=True)
 
         yield cursor
 
-        # Commit only if requested and no exceptions occurred
         if commit:
             connection.commit()
 
     except Exception as e:
-        # If any error occurs, rollback the transaction
         if connection:
             connection.rollback()
-        raise e  # Re-raise the exception
+        raise e
     finally:
-        # Always close cursor and connection
         if cursor:
             cursor.close()
         if connection:
